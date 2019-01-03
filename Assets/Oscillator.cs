334f57a767f9a434abbe0f2ca782b9c7041abdd7
STU\Mikko <mikkoruot@gmail.com>
2019-01-03 18:02:24 +0200

    Added oscillator basis

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+
+[DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
+    [SerializeField] Vector3 movementVector;
+
+    [Range(0,1)]
+    [SerializeField]
+    float moveFactor; //0 for not moved 1 if moved
+
+    Vector3 startingPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //set movementfactor automatically
+
+        Vector3 displacement = moveFactor * movementVector;
+        transform.position = startingPos + displacement;
     }
 }
